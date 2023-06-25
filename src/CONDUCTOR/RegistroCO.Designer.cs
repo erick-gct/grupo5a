a3e01081327a0a34453c91bff9341620447e6514
Erick Campuzano <89644016+erick-gct@users.noreply.github.com>
2023-06-24 20:55:35 -0500

    Add files via upload

---
+++ src/CONDUCTOR/RegistroCO.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace CONDUCTOR
+{
+    partial class RegistroCO
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtservicios = new System.Windows.Forms.TextBox();
+            this.txtestado = new System.Windows.Forms.TextBox();
+            this.txtdireccion = new System.Windows.Forms.TextBox();
+            this.txtfnacimiento = new System.Windows.Forms.TextBox();
+            this.txtcbancaria = new System.Windows.Forms.TextBox();
+            this.txttelefono = new System.Windows.Forms.TextBox();
+            this.txtcedula = new System.Windows.Forms.TextBox();
+            this.txtnombres = new System.Windows.Forms.TextBox();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtservicios
+            // 
+            this.txtservicios.Location = new System.Drawing.Point(524, 351);
+            this.txtservicios.Name = "txtservicios";
+            this.txtservicios.Size = new System.Drawing.Size(177, 20);
+            this.txtservicios.TabIndex = 61;
+            this.txtservicios.TextChanged += new System.EventHandler(this.txtservicios_TextChanged);
+            // 
+            // txtestado
+            // 
+            this.txtestado.Location = new System.Drawing.Point(278, 348);
+            this.txtestado.Name = "txtestado";
+            this.txtestado.Size = new System.Drawing.Size(152, 20);
+            this.txtestado.TabIndex = 60;
+            this.txtestado.TextChanged += new System.EventHandler(this.txtestado_TextChanged);
+            // 
+            // txtdireccion
+            // 
+            this.txtdireccion.Location = new System.Drawing.Point(254, 294);
+            this.txtdireccion.Name = "txtdireccion";
+            this.txtdireccion.Size = new System.Drawing.Size(447, 20);
+            this.txtdireccion.TabIndex = 59;
+            this.txtdireccion.TextChanged += new System.EventHandler(this.txtdireccion_TextChanged);
+            // 
+            // txtfnacimiento
+            // 
+            this.txtfnacimiento.Location = new System.Drawing.Point(223, 240);
+            this.txtfnacimiento.Name = "txtfnacimiento";
+            this.txtfnacimiento.Size = new System.Drawing.Size(152, 20);
+            this.txtfnacimiento.TabIndex = 58;
+            this.txtfnacimiento.TextChanged += new System.EventHandler(this.txtfnacimiento_TextChanged);
+            // 
+            // txtcbancaria
+            // 
+            this.txtcbancaria.Location = new System.Drawing.Point(646, 186);
+            this.txtcbancaria.Name = "txtcbancaria";
+            this.txtcbancaria.Size = new System.Drawing.Size(55, 20);
+            this.txtcbancaria.TabIndex = 57;
+            this.txtcbancaria.TextChanged += new System.EventHandler(this.txtcbancaria_TextChanged);
+            // 
+            // txttelefono
+            // 
+            this.txttelefono.Location = new System.Drawing.Point(381, 186);
+            this.txttelefono.Name = "txttelefono";
+            this.txttelefono.Size = new System.Drawing.Size(132, 20);
+            this.txttelefono.TabIndex = 56;
+            this.txttelefono.TextChanged += new System.EventHandler(this.txttelefono_TextChanged);
+            // 
+            // txtcedula
+            // 
+            this.txtcedula.Location = new System.Drawing.Point(156, 186);
+            this.txtcedula.Name = "txtcedula";
+            this.txtcedula.Size = new System.Drawing.Size(139, 20);
+            this.txtcedula.TabIndex = 55;
+            this.txtcedula.TextChanged += new System.EventHandler(this.txtcedula_TextChanged);
+            // 
+            // txtnombres
+            // 
+            this.txtnombres.Location = new System.Drawing.Point(465, 136);
+            this.txtnombres.Name = "txtnombres";
+            this.txtnombres.Size = new System.Drawing.Size(236, 20);
+            this.txtnombres.TabIndex = 54;
+            this.txtnombres.TextChanged += new System.EventHandler(this.txtnombres_TextChanged);
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Enabled = false;
+            this.txtcodigo.Location = new System.Drawing.Point(243, 136);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(124, 20);
+            this.txtcodigo.TabIndex = 53;
+            this.txtcodigo.TextChanged += new System.EventHandler(this.txtcodigo_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(453, 353);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 15);
+            this.label10.TabIndex = 52;
+            this.label10.Text = "Servicio:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(90, 350);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(182, 15);
+            this.label9.TabIndex = 51;
+            this.label9.Text = "Estado del Conductor (O/L)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(90, 296);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(158, 15);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Dirección Domiciliaria:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(90, 242);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 15);
+            this.label7.TabIndex = 49;
+            this.label7.Text = "Fecha nacimiento:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(90, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(152, 15);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Código del Conductor:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(528, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 15);
+            this.label5.TabIndex = 47;
+            this.label5.Text = "días libre al mes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(305, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 15);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Teléfono: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(90, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 15);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Cédula: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(389, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Nombres:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(144)))), ((int)(((byte)(129)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(607, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 47);
+            this.button1.TabIndex = 62;
+            this.button1.Text = "Registrar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DimGray;
+            this.button2.Location = new System.Drawing.Point(460, 428);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 47);
+            this.button2.TabIndex = 63;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // RegistroCO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::CONDUCTOR.Properties.Resources.CONBG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 504);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtservicios);
+            this.Controls.Add(this.txtestado);
+            this.Controls.Add(this.txtdireccion);
+            this.Controls.Add(this.txtfnacimiento);
+            this.Controls.Add(this.txtcbancaria);
+            this.Controls.Add(this.txttelefono);
+            this.Controls.Add(this.txtcedula);
+            this.Controls.Add(this.txtnombres);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "RegistroCO";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.RegistroCO_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtservicios;
+        private System.Windows.Forms.TextBox txtestado;
+        private System.Windows.Forms.TextBox txtdireccion;
+        private System.Windows.Forms.TextBox txtfnacimiento;
+        private System.Windows.Forms.TextBox txtcbancaria;
+        private System.Windows.Forms.TextBox txttelefono;
+        private System.Windows.Forms.TextBox txtcedula;
+        private System.Windows.Forms.TextBox txtnombres;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ src/CONDUCTOR/RegistroCO.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Capa_Entidad;
+using Capa_Negocio;
+namespace CONDUCTOR
+{
+    public partial class RegistroCO : Form
+    {
+        ClaseEntidad objent = new ClaseEntidad();
+        ClaseNegocio objneg = new ClaseNegocio();
+        public RegistroCO()
+        {
+            InitializeComponent();
+        }
+
+        void mantenimiento(String accion)
+        {
+            objent.codigo2 = txtcodigo.Text;
+            objent.cbancaria = txtcbancaria.Text;
+            objent.cedula = txtcedula.Text;
+            objent.direccion = txtdireccion.Text;
+            objent.estado = txtestado.Text;
+            objent.fnacimiento = txtfnacimiento.Text;
+            objent.nombres = txtnombres.Text;
+            objent.servicios = txtservicios.Text;
+            objent.telefono = txttelefono.Text;
+            objent.accion2 = accion;
+            String men = objneg.N_mantenimiento_conductor(objent);
+            MessageBox.Show(men, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        void limpiar()
+        {
+            txtcodigo.Text = "";
+            txtcbancaria.Text = "";
+            txtcedula.Text = "";
+            txtdireccion.Text = "";
+            txtestado.Text = "";
+            txtfnacimiento.Text = "";
+            txtnombres.Text = "";
+            txtservicios.Text = "";
+            txttelefono.Text = "";
+
+
+        }
+        private void RegistroCO_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void iconButton2_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+
+        private void txtcodigo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtnombres_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcedula_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txttelefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcbancaria_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtestado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtfnacimiento_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtdireccion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtservicios_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconPictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Deseas registrar a " + txtnombres.Text + "?", "Mensaje",
+                   MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+            {
+                mantenimiento("1");
+                limpiar();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
